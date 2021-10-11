dc38a54f92f5cea649984de80ce1b24197c19ec3
John T. Wodder II <git@varonathe.org>
2021-10-10 23:57:53 -0400

    Added a utility/maintenance script

---
+++ util/merge-words.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+set -ex -o pipefail
+
+REPO_WORDS="$(dirname "$0")"/../dotfiles/share/spell/words.utf-8.add
+HOME_WORDS="$HOME/share/spell/words.utf-8.add"
+TMP_WORDS="$(mktemp)"
+
+LC_ALL=C.UTF-8 sort -o "$TMP_WORDS" -u "$REPO_WORDS" "$HOME_WORDS"
+
+cp -f "$TMP_WORDS" "$REPO_WORDS"
+cp -f "$TMP_WORDS" "$HOME_WORDS"
+rm "$TMP_WORDS"
+
+make -C ~/share/spell
