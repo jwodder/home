e49220ae53ce4f13c29e784ec9696429247a1e2a
John T. Wodder II <git@varonathe.org>
2024-07-11 11:21:28 +0000

    install-rust.sh: Also install rust-analyzer on Linux

--- util/install-rust.sh
+++ util/install-rust.sh
@@ -7,3 +7,13 @@ curl --proto '=https' --tlsv1.2 -LsSf https://sh.rustup.rs \
 curl --proto '=https' --tlsv1.2 -LsSf \
     https://raw.githubusercontent.com/cargo-bins/cargo-binstall/main/install-from-binstall-release.sh \
     | bash
+
+if [ "$(uname)" = Linux ]
+then
+    # <https://rust-analyzer.github.io/manual.html#rust-analyzer-language-server-binary>
+    mkdir -p ~/.local/bin
+    curl --proto '=https' --tlsv1.2 -LsSf \
+        https://github.com/rust-lang/rust-analyzer/releases/latest/download/rust-analyzer-"$(arch)"-unknown-linux-gnu.gz \
+        | gunzip -c - > ~/.local/bin/rust-analyzer
+    chmod +x ~/.local/bin/rust-analyzer
+fi
