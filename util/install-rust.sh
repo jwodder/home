ba87f226ffa0c9b0f7247d9fd493ebf408b8ea5d
John T. Wodder II <git@varonathe.org>
2025-11-18 09:22:43 -0500

    Install rust-analyzer via rustup

--- util/install-rust.sh
+++ util/install-rust.sh
@@ -8,12 +8,4 @@ curl --proto '=https' --tlsv1.2 -LsSf \
     https://raw.githubusercontent.com/cargo-bins/cargo-binstall/main/install-from-binstall-release.sh \
     | bash
 
-if [ "$(uname)" = Linux ]
-then
-    # <https://rust-analyzer.github.io/manual.html#rust-analyzer-language-server-binary>
-    mkdir -p ~/.local/bin
-    curl --proto '=https' --tlsv1.2 -LsSf \
-        https://github.com/rust-lang/rust-analyzer/releases/latest/download/rust-analyzer-"$(arch)"-unknown-linux-gnu.gz \
-        | gunzip -c - > ~/.local/bin/rust-analyzer
-    chmod +x ~/.local/bin/rust-analyzer
-fi
+rustup component add rust-analyzer
